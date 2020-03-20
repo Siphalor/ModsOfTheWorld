d0214f16cb610c5eb995dc5697f2d0b96d8ebdb5
Siphalor <info@siphalor.de>
2020-03-20 10:13:04 +0100

    Version 1.0.5
    * Update mixin json
    * Nether Update ready!

--- src/main/java/de/siphalor/modsoftheworld/client/mixin/MainMenuScreenMixin.java
+++ src/main/java/de/siphalor/modsoftheworld/client/mixin/MainMenuScreenMixin.java
@@ -35,13 +35,13 @@ public abstract class MainMenuScreenMixin extends Screen {
 	}
 
 	@SuppressWarnings("ConstantConditions")
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;blit(IIFFIIII)V", ordinal = 0))
+	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/TitleScreen;drawTexture(IIFFIIII)V", ordinal = 0))
 	public void editionBlitProxy(int x, int y, float texX, float texY, int width, int height, int texWidth, int texHeight) {
-		blit(x + 39, y, texX + 39, texY, width - 39, height, texWidth, texHeight);
+		drawTexture(x + 39, y, texX + 39, texY, width - 39, height, texWidth, texHeight);
 
 		if(modsOfTheWorld_logoTime > ClientCore.WHOLE_TIME) {
 			modsOfTheWorld_currentLogo = modsOfTheWorld_currentLogo >= ClientCore.getLogos().size() - 1 ? 0 : modsOfTheWorld_currentLogo + 1;
-			GLFW.glfwSetWindowTitle(minecraft.getWindow().getHandle(), "Minecraft " + SharedConstants.getGameVersion().getName() + " - " + ClientCore.getLogos().get(modsOfTheWorld_currentLogo).modName + " Edition");
+			GLFW.glfwSetWindowTitle(client.getWindow().getHandle(), "Minecraft " + SharedConstants.getGameVersion().getName() + " - " + ClientCore.getLogos().get(modsOfTheWorld_currentLogo).modName + " Edition");
 			splashText = ClientCore.getLogos().get(modsOfTheWorld_currentLogo).splashProvider.get();
 		}
 		modsOfTheWorld_logoTime %= ClientCore.WHOLE_TIME;
@@ -58,12 +58,12 @@ public abstract class MainMenuScreenMixin extends Screen {
 		RenderSystem.pushMatrix();
 
 		Logo logoTexture = ClientCore.getLogos().get(modsOfTheWorld_currentLogo);
-		minecraft.getTextureManager().bindTexture(logoTexture.identifier);
+		client.getTextureManager().bindTexture(logoTexture.identifier);
 
 		float scaleFactor = (float) height / (float) logoTexture.height;
 		RenderSystem.translatef(x + 39F - logoTexture.width * scaleFactor, y, 0F);
 		RenderSystem.scalef(scaleFactor, scaleFactor, 1F);
-		blit(0, 0, 0.0F, 0.0F, logoTexture.width, logoTexture.height, logoTexture.width, logoTexture.height);
+		drawTexture(0, 0, 0.0F, 0.0F, logoTexture.width, logoTexture.height, logoTexture.width, logoTexture.height);
 
         if(color[3] == 1.0F)
         	RenderSystem.color4f(1F, 1F, 1F, 1F);
